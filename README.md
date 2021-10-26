214c0db6f9047467d43dc9708870541507f9c811
Muhammad Reza Irvanda <muzavan@gmail.com>
2021-10-27 00:29:25 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# WWE Ranking
+
+## Description
+This repository will store and generate un-official ranking of WWE Superstars. The ranking will be determined by using [Elo Rating System](https://www.hackerearth.com/blog/developers/elo-rating-system-common-link-facemash-chess/) (you might recognize it if you watch The Social Network film. It's the algorithm taught by Eduard Saverin to Mark Zuckeberg when he wants to create Facemash).
+
+The latest ranking can be accessed [here](latest_rating.csv).
+
+## Assumption and Rule
+The assumption and rule applied to this ranking are determined as follows.
+- The rating will be based on the win/loss record. Since I use Elo Rating System, the rate is not solely determined by the number of win or loss. How meaningful the win/loss will affect the rating as well (e.g. lower-tier wrestler that wins against high-tier wrestler will get a better boost rather than wins against the same-tier). Win/loss is only recorded if it happens in a match. So, if A pins B and then B beats up A until B stands tall in the final moment, the record will still show that A is the winner and B is the loser.
+
+- Every wrestler will start with a base rating score of 2500. Higher rating means higher ranking (higher meaningful wins). The score will be reset after every draft (including single draft).
+
+- DQ and Non Finish will be considered as draw
+
+- A match with multiple participant at the same time will be considered as multiple match that consist of the winner and the losers. For example, if there is a Triple Threat Match betweem A, B, and C with B winning, the rating will be updated as if we have two match: A vs C(winner) and B vs C(winner). Note that Gauntlet match will be considered as a normal match since only two competitors wrestle at the same time.
+
+- Only televised match (including PPV) are considered to update the ranking. Meaning that the ranking will be updated after RAW, Smackdown, and PPV (for now, I will exclude NXT since I do not follow it closely, help appreciated :p)
+
+- Each tag team / faction / stable members will have their own rating. So, the rating for them might be different (since sometimes they book a 1-on-1 match to serve the feud)
+
+- These assumptions and rule might be updated later. I create this just for fun, do not take it seriously! In the end, WWE product is a form of entertainment. We seek a pleasure by watching/discussing it.
+
---
+++ script.py
@@ -0,0 +1,99 @@
+import csv
+from dataclasses import dataclass
+import math
+import sys
+from typing import Mapping
+
+@dataclass
+class Wrestler:
+    name: str
+    rating: float
+    brand: str # RAW or Smackdown
+    win: int
+    loss: int
+    total: int
+
+# Use the same as chess?
+K_FACTOR = 24
+
+def expectation(r1, r2):
+    return (1.0 / (1.0 + math.pow(10, r2 - r1)/400))
+
+def update(winner: Wrestler, loser: Wrestler):
+    e_win = expectation(winner.rating, loser.rating)
+    e_loss = expectation(loser.rating, winner.rating)
+
+    winner.rating = (winner.rating + K_FACTOR * (1 - e_win))
+    loser.rating = (loser.rating + K_FACTOR * (0 - e_loss))
+
+LATEST_RATING_CSV = "latest_rating.csv"
+def get_latest_rating() -> Mapping[str, Wrestler]:
+    wrestlers = dict()
+    with open(LATEST_RATING_CSV, "r") as f:
+        reader = csv.DictReader(f)
+        for r in reader:
+            w = Wrestler(r["name"], r["rating"], r["brand"], 
+                            r["win"], r["loss"], r["total"])
+
+            if w.name in wrestlers:
+                print("Duplicate wrestler:", w.name)
+                exit()
+            wrestlers[w.name] = w
+
+    return wrestlers
+
+def update_from_episode(episode_file: str, latest_rating: Mapping[str, Wrestler]):
+    with open(episode_file, "r") as f:
+        reader = csv.DictReader(f)
+        for r in reader:
+            winners = r["winners"].strip().split("&")
+            losers = r["losers"].strip().split("&")
+
+            for w_name in winners:
+                for l_name in losers:
+                    if w_name not in latest_rating:
+                        print("Unregonized wrestler:", w_name)
+                        exit()
+                    if l_name not in latest_rating:
+                        print("Unrecognized wrestler:", l_name)
+                        exit()
+
+                    winner = latest_rating[w_name]
+                    loser = latest_rating[l_name]
+
+                    update(winner, loser)
+
+    
+
+
+def dump_latest_rating(latest_rating: Mapping[str, Wrestler]):
+    wrestlers = []
+    for _, w in latest_rating.items():
+        wrestlers.append(w)
+
+    wrestlers = sorted(wrestlers, key= lambda w: (w.brand, -w.rating, w.name))
+
+    with open(LATEST_RATING_CSV, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name", "rating", "brand", "win", "loss", "total"])
+        rows = [(w.name, w.rating, w.brand, w.win, w.loss, w.total) for w in wrestlers]
+        writer.writerows(rows)
+
+
+# Usage: python script.py episode.csv
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pass the episode file")
+        exit()
+
+    episode_files = sys.argv[1:]
+    latest_rating = get_latest_rating()
+
+    for episode in episode_files:
+        print("Process episode:", episode)
+        update_from_episode(episode, latest_rating)
+
+    dump_latest_rating(latest_rating)
+    print("Done!")
+
+
